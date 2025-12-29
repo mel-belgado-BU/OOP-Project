db9d2cfc149cb6af67ed5dc7ecda2f54eb2a8cb8
mel-belgado-BU <meljullianbelardo.belgado24@bicol-u.edu.ph>
2025-12-29 22:06:49 +0800

    stuff

--- project/src/main/java/RestaurantGUI.java
+++ project/src/main/java/RestaurantGUI.java
@@ -3,6 +3,23 @@ import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.util.ArrayList;
 
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSeparator;
+import javax.swing.JTable;
+import javax.swing.JTextArea;
+import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.table.DefaultTableModel;
+
 public class RestaurantGUI extends JFrame {
     private Restaurant restaurant;
     private JPanel currentPanel;
